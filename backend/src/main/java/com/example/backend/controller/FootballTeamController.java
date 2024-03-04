5bf35d8c23a9281b9adbb66a5bfeda30a933beee
Noic Noic <galanisd@csd.auth.gr>
2024-03-04 13:49:52 +0200

    Rest API

--- backend/src/main/java/com/example/backend/controller/FootballTeamController.java
+++ backend/src/main/java/com/example/backend/controller/FootballTeamController.java
@@ -3,14 +3,16 @@ package com.example.backend.controller;
 
 import com.example.backend.entity.FootballTeam;
 import com.example.backend.service.FootballTeamService;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.net.URI;
+import java.util.Collection;
 
 @RestController
 @RequiredArgsConstructor
@@ -18,8 +20,29 @@ import java.net.URI;
 public class FootballTeamController {
     private final FootballTeamService footballTeamService;
 
+    @GetMapping("")
+    public String HelloWorld() {
+        return " Hello world !!" ;
+    }
+
+    @GetMapping("/footballteams")
+    public ResponseEntity<Collection<FootballTeam>> getAllTeams() {
+        return ResponseEntity.ok().body(footballTeamService.getAllTeams());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<FootballTeam> getFootballTeam(@PathVariable String id) {
+        return ResponseEntity.ok().body(footballTeamService.getFootballTeam(id));
+    }
+
     @PostMapping
     public ResponseEntity<FootballTeam> createFootballTeam(@RequestBody FootballTeam footballTeam) {
         return ResponseEntity.created(URI.create("/footballteam/ID")).body(footballTeamService.createFootballTeam(footballTeam));
     }
+
+   @DeleteMapping("/{id}")
+    public void deleteFootballTeam(@PathVariable String id) {
+       footballTeamService.deleteFootballTeam(id);
+   }
+
 }
--- backend/src/main/java/com/example/backend/service/FootballTeamService.java
+++ backend/src/main/java/com/example/backend/service/FootballTeamService.java
@@ -2,17 +2,49 @@ package com.example.backend.service;
 
 import com.example.backend.entity.FootballTeam;
 import com.example.backend.repository.FootballTeamRepository;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+
+
+import java.util.List;
 
 @Service
 public class FootballTeamService {
     private final FootballTeamRepository footballTeamRepository;
 
+
+    /**
+     * @param footballTeamRepository
+     * Constructor
+     * Dependency Injection
+     */
     public FootballTeamService(FootballTeamRepository footballTeamRepository) {
         this.footballTeamRepository = footballTeamRepository;
     }
 
+    public List<FootballTeam> getAllTeams() {
+        return footballTeamRepository.findAll();
+    }
+
+
+    public FootballTeam getFootballTeam(String id) {
+        return footballTeamRepository.findById(id).orElseThrow(()-> new ResponseStatusException(HttpStatus.NOT_FOUND));
+    }
+
+
     public FootballTeam createFootballTeam(FootballTeam footballTeam) {
         return footballTeamRepository.save(footballTeam);
     }
+
+
+    public void deleteFootballTeam(String id) {
+        footballTeamRepository.deleteById(id);
+    }
+
+
+
+
+
 }
